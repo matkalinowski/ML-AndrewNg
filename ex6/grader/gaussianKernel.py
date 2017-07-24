aff7f04c8d31a540b61be044a3273400b6de31ea
Mateusz Kalinowski <mateusz.a.kalinowski@gmail.com>
2017-07-24 15:20:13 +0200

    Exercise 6. Init. GaussianKernel Done with grader.

---
+++ ex6/grader/gaussianKernel.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def gaussianKernel(x1, x2, sigma):
+    """returns a gaussian kernel between x1 and x2
+    and returns the value in sim
+    """
+
+# Ensure that x1 and x2 are column vectors
+#     x1 = x1.ravel()
+#     x2 = x2.ravel()
+
+# You need to return the following variables correctly.
+    sim = 0
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Fill in this function to return the similarity between x1
+#               and x2 computed using a Gaussian kernel with bandwidth
+#               sigma
+#
+#
+
+
+# =============================================================
+    distance = np.sum(np.power(x1 - x2, 2))
+    return np.exp(-distance / (2 * np.power(sigma, 2) ) )
